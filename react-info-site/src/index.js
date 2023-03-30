7fd571f5fa2003f53d9b502507c0c9b9c21b9cf2
Manish Sah <manish@tech.local>
2023-03-30 21:19:52 +0545

    create first react component

--- react-info-site/src/index.js
+++ react-info-site/src/index.js
@@ -3,22 +3,24 @@ import ReactDOM from 'react-dom/client';
 import logo from './logo.svg';
 import reportWebVitals from './reportWebVitals';
 
-const page = (
-  <div>
+function Page(){
+  return (
+    <div>
     <img src={logo} width="40px"/>
     <h1>Fun facts about React</h1>
     <ul>
-      <li>Was released in 2013</li>
+      <li>Was released in 2013.</li>
       <li>Was originally created by Jordan Walke</li>
       <li>Has well over 100K stars on Git</li>
       <li>Is Maintained by Facebook</li>
       <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
   </div>
-)
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-page
+<Page />
 );
 
 reportWebVitals();
