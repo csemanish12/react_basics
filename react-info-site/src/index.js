33576411b2f3e27a5e83e42de2ab3ba34f661409
Manish Sah <manish@tech.local>
2023-03-30 21:04:41 +0545

    create html section for react info site

---
+++ react-info-site/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import logo from './logo.svg';
+import reportWebVitals from './reportWebVitals';
+
+const page = (
+  <div>
+    <img src={logo} width="40px"/>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Git</li>
+      <li>Is Maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+page
+);
+
+reportWebVitals();
