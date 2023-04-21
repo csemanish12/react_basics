b1aa4c8465b01243cdd25fa421af6734322ef1a2
Manish Sah <manish@tech.local>
2023-04-21 17:27:58 +0545

    create state for meme image

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,11 +1,26 @@
+import React from "react"
+import memesData from "./MemeData"
+
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        console.log("number:", randomNumber)
+        setMemeImage(memesArray[randomNumber].url)
+
+    }
+
+
     return (
         <main className="main">
-        <form className="form">
+        <div className="form">
                 <input  type='text' placeholder='Top Text' className="form--input"/>
                 <input type='text' placeholder='Bottom Text' className="form--input"/>
-            <button type='submit' className="form--button">Get a new meme image 🖼</button>
-        </form>
+            <button type='submit' className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+            <img className="meme--image" src={memeImage}  />
+        </div>
         </main>
     )
 }
