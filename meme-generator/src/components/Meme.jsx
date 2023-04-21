8fed15e3378fc615b81747f02522a9d38dc3386c
Manish Sah <manish@tech.local>
2023-04-21 20:34:12 +0545

    refactor meme component * replace memeImage state with meme state with additional attributes * use allMemeImage state variable to hold all the memes data

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,13 +2,23 @@ import React from "react"
 import memesData from "./MemeData"
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImage, setAllMemeImage] = React.useState(memesData)
 
     function getMemeImage(){
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImage.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        console.log("number:", randomNumber)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: url
+            }
+        })
 
     }
 
@@ -19,7 +29,7 @@ export default function Meme() {
                 <input  type='text' placeholder='Top Text' className="form--input"/>
                 <input type='text' placeholder='Bottom Text' className="form--input"/>
             <button type='submit' className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
-            <img className="meme--image" src={memeImage}  />
+            <img className="meme--image" src={meme.randomImage}  />
         </div>
         </main>
     )
